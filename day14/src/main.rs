89a713febaab1facc52d27d2daf349788a1f2e4e
Sebastian Nocke <sjno@gmx.de>
2022-12-18 14:48:40 +0100

    Highly non functional code for day 14 part one. But hey it looks kinda funny

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -22,8 +22,29 @@ fn part_one() {
 mod tests {
     use super::*;
 
+    const TEST_INPUT: &str = r"498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9
+";
+
+    #[test]
+    fn parse_test() {
+        let rock_shapes: Vec<RockShape> = TEST_INPUT.split("\n").filter(|line| !line.is_empty()).map(|line| line.parse().unwrap()).collect();
+        let known_rock_shapes = vec![RockShape::new(vec![Position(498, 4), Position(498, 6), Position(496, 6)]),
+            RockShape::new(vec![Position(503, 4), Position(502, 4), Position(502, 9), Position(494, 9)])];
+
+        assert_eq!(rock_shapes.len(), known_rock_shapes.len());
+        assert_eq!(rock_shapes[0], known_rock_shapes[0]);
+        assert_eq!(rock_shapes[1], known_rock_shapes[1]);
+
+        let cave = input_to_cave(TEST_INPUT);
+
+        assert_eq!(cave.left_bound(), 494);
+        assert_eq!(cave.right_bound(), 503);
+        assert_eq!(cave.lower_bound(), 9);
+    }
+
     #[test]
     fn part_one_test() {
-
+        let cave = input_to_cave(TEST_INPUT);
     }
 }
--- day14/src/puzzle.rs
+++ day14/src/puzzle.rs
@@ -1,17 +1,73 @@
-use std::str::FromStr;
+use std::{str::FromStr, collections::HashSet};
 
-pub struct Position(pub u64, pub u64);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Position(pub usize, pub usize);
+
+impl Position {
+    pub fn line_between(&self, other: &Self) -> Vec<Position> {
+        if self.0 == other.0 {
+            let range = if self.1 > other.1 {
+                other.1+1..self.1-1
+            } else {
+                self.1..other.1
+            };
+            dbg!(&range);
+            range.into_iter().map(|num| Position(self.0, num)).collect()
+        } else {
+            let range = if self.0 > other.0 {
+                other.0+1..self.0-1
+            } else {
+                self.0..other.0
+            };
+            dbg!(&range);
+            range.into_iter().map(|num| Position(num, self.1)).collect()
+        }
+    }
+}
 
 impl FromStr for Position {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Err(())
+        let parts: Vec<&str> = s.split(",").collect();
+
+        if parts.len() == 2 {
+            Ok(Position(parts[0].parse().unwrap(), parts[1].parse().unwrap()))
+        } else {
+            Err(())
+        }
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct RockShape {
     line: Vec<Position>
+}
+
+impl RockShape {
+    pub fn new(line: Vec<Position>) -> Self {
+        RockShape { line }
+    }
+
+    pub fn rock_positions(&self) -> Vec<Position> {
+
+        if self.line.len() == 1 {
+            vec![self.line[0]]
+        } else {
+            let mut collected_positions = Vec::new();
+
+            for i in 0..self.line.len() - 1 {
+                let line = self.line[i].line_between(&self.line[i + 1]);
+                dbg!(&line);
+                for element in line {
+                    collected_positions.push(element);
+                }
+            }
+            collected_positions.push(self.line[self.line.len() - 1]);
+
+            collected_positions
+        }
+    }
 }
 
 impl FromStr for RockShape {
@@ -23,18 +79,50 @@ impl FromStr for RockShape {
     }
 }
 
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Filling {
     Air,
     Rock,
     Sand,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Cave {
-    area: Vec<Vec<Filling>>
+    left_bound: usize,
+    right_bound: usize,
+    lower_bound: usize,
+    area: Vec<Vec<Filling>>,
 }
 
 impl Cave {
     pub fn new(rock_shapes: Vec<RockShape>) -> Self {
-        todo!()
+        let mut rock_set = HashSet::new();
+
+        for rock_shape in rock_shapes {
+            for rock_position in rock_shape.rock_positions() {
+                rock_set.insert(rock_position);
+            }
+        }
+
+        let left_bound = rock_set.iter().map(|pos| pos.0).min().unwrap();
+        let right_bound = rock_set.iter().map(|pos| pos.0).max().unwrap();
+        let lower_bound = rock_set.iter().map(|pos| pos.1).max().unwrap();
+        let width = right_bound - left_bound + 1;
+
+        let area = vec![vec![Filling::Air; width]; lower_bound];
+
+        Self { left_bound, right_bound, lower_bound, area }
+    }
+
+    pub fn left_bound(&self) -> usize {
+        self.left_bound
+    }
+
+    pub fn right_bound(&self) -> usize {
+        self.right_bound
+    }
+
+    pub fn lower_bound(&self) -> usize {
+        self.lower_bound
     }
 }
