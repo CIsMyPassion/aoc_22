026d5c536695389b5bbff5319de658bff1c91d5a
Sebastian Nocke <sjno@gmx.de>
2022-12-25 01:21:52 +0100

    Started on day 17 part one

---
+++ day17/src/main.rs
@@ -0,0 +1,39 @@
+use puzzle::*;
+
+mod puzzle;
+
+fn main() {
+    part_one();
+}
+
+fn part_one() {
+    let input = day_util::read_input_safe("day17");
+    let push_list = PushDirection::parse_to_vec(&input);
+    let mut chamber = Chamber::new(7, push_list);
+        
+    while chamber.dropped_rocks() < 2022 {
+        chamber.drop_rock();
+    }
+    
+    println!("Highest rock: {}", chamber.highest_rock());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    const TEST_INPUT: &str = r#">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>
+"#;
+
+    #[test]
+    fn part_one_test() {
+        let push_list = PushDirection::parse_to_vec(TEST_INPUT);
+        let mut chamber = Chamber::new(7, push_list);
+        
+        while chamber.dropped_rocks() < 11 {
+            chamber.drop_rock();
+        }
+        
+        assert_eq!(chamber.highest_rock(), 17);
+    }
+}
---
+++ day17/src/puzzle/mod.rs
@@ -0,0 +1,203 @@
+use std::{collections::HashSet, ops::Add};
+
+#[derive(Copy, Clone)]
+pub enum PushDirection {
+    Left,
+    Right,
+}
+
+impl PushDirection {
+    pub fn parse_to_vec(s: &str) -> Vec<Self> {
+        s.chars().flat_map(|c| {
+            match c {
+                '<' => Some(PushDirection::Left),
+                '>' => Some(PushDirection::Right),
+                _ => None,
+            }
+        }).collect()
+    }
+}
+
+enum RockShape {
+    HorizontalLine,
+    Plus,
+    BackwardsL,
+    VerticalLine,
+    Cube,
+}
+
+impl RockShape {
+    pub fn from_index(index: u64) -> Self {
+        match (index - 1) % 5 {
+            0 => RockShape::HorizontalLine,
+            1 => RockShape::Plus,
+            2 => RockShape::BackwardsL,
+            3 => RockShape::VerticalLine,
+            _ => RockShape::Cube,
+        }
+    }
+    
+    pub fn shape(&self) -> HashSet<Position> {
+        match self {
+            Self::HorizontalLine => HashSet::from([
+                Position::new(0, 0),
+                Position::new(0, 1),
+                Position::new(0, 2),
+                Position::new(0, 3),
+            ]),
+            Self::Plus => HashSet::from([
+                Position::new(0, 1),
+                Position::new(1, 0),
+                Position::new(1, 1),
+                Position::new(1, 2),
+                Position::new(2, 1),
+            ]),
+            Self::BackwardsL => HashSet::from([
+                Position::new(0, 0),
+                Position::new(0, 1),
+                Position::new(0, 2),
+                Position::new(1, 2),
+                Position::new(2, 2),
+            ]),
+            Self::VerticalLine => HashSet::from([
+                Position::new(0, 0),
+                Position::new(1, 0),
+                Position::new(2, 0),
+                Position::new(3, 0),
+            ]),
+            Self::Cube => HashSet::from([
+                Position::new(0, 0),
+                Position::new(1, 0),
+                Position::new(0, 1),
+                Position::new(1, 1),
+            ]),
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+struct Position {
+    row: u64,
+    column: u64,
+}
+
+impl Position {
+    pub fn new(row: u64, column: u64) -> Self {
+        Self { row, column }
+    }
+}
+
+impl Add for Position {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            row: self.row + rhs.row,
+            column: self.column + rhs.column,
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+enum Filling {
+    Rock,
+    Air,
+}
+
+pub struct Chamber {
+    width: u64,
+    filled_space: Vec<Vec<Filling>>,
+    dropped_rocks: u64,
+    push_list: Vec<PushDirection>,
+    drop_counter: u64,
+}
+
+impl Chamber {
+    pub fn new(width: u64, push_list: Vec<PushDirection>) -> Self {
+        Self { width, filled_space: Vec::new(), dropped_rocks: 0, push_list, drop_counter: 0 }
+    }
+    
+    pub fn drop_rock(&mut self) {
+        self.dropped_rocks += 1;
+        let rock = RockShape::from_index(self.dropped_rocks);
+        let rock_shape = rock.shape();
+        let mut rock_position = Position::new(self.highest_rock() + 3, 2);
+
+        let width = rock_shape.iter().map(|pos| pos.column).max().unwrap();
+        loop {
+            let push_direction = self.current_push_direction();
+            self.drop_counter += 1;
+            
+            match push_direction {
+                PushDirection::Left => {
+                    if rock_position.column > 0 {
+                        rock_position.column -= 1;
+                    }
+                    
+                    if self.collides(&rock_position, &rock_shape) {
+                        rock_position.column += 1;
+                    }
+                },
+                PushDirection::Right => {
+                    if rock_position.column + width < self.width - 1 {
+                        rock_position.column += 1;
+                    }
+                    
+                    if self.collides(&rock_position, &rock_shape) {
+                        rock_position.column -= 1;
+                    }
+                },
+            };
+            
+            if rock_position.row > 0 {
+                rock_position.row -= 1;
+                if !self.collides(&rock_position, &rock_shape) {
+
+                } else {
+                    self.set_rock(&rock_position, &rock_shape);
+                    break;
+                }
+            } else {
+                self.set_rock(&rock_position, &rock_shape);
+                break;
+            }
+            
+        }
+    }
+
+    fn collides(&self, position: &Position, shape: &HashSet<Position>) -> bool {
+        if position.row >= self.filled_space.len() as u64 {
+            false
+        } else {
+            true
+        }
+    }
+
+    fn set_rock(&mut self, position: &Position, shape: &HashSet<Position>) {
+        let height = shape.iter().map(|pos| pos.row).max().unwrap();
+        let total_height = position.row + height;
+        for _ in self.filled_space.len() as u64..total_height {
+            self.filled_space.push(vec![Filling::Air; self.width as usize]);
+        }
+        
+        shape.iter().for_each(|pos| {
+            let total_position = *pos + *position;
+            self.filled_space[total_position.row as usize][total_position.column as usize] = Filling::Rock;
+        });
+
+        todo!("set rock into chamber")
+    }
+
+    fn current_push_direction(&self) -> PushDirection {
+        let index = self.drop_counter as usize % self.push_list.len();
+        self.push_list[index]
+    }
+
+    pub fn dropped_rocks(&self) -> u64 {
+        self.dropped_rocks
+    }
+    
+    pub fn highest_rock(&self) -> u64 {
+        self.filled_space.len() as u64
+    }
+}
