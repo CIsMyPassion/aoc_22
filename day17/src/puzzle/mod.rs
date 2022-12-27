76801d744f6b632d22ab7c65c6cd3cd32ef261aa
Sebastian Nocke <sjno@gmx.de>
2022-12-28 00:45:05 +0100

    Slowly on the right way

--- day17/src/puzzle/mod.rs
+++ day17/src/puzzle/mod.rs
@@ -118,8 +118,6 @@ pub enum Filling {
 pub fn height_for_dropped_rocks(chamber: &mut Chamber, push_list: Vec<PushDirection>, rock_count: u64) -> u64 {
     let mut dropped_rocks = 0;
     let mut jet_index = 0;
-    let mut found_patterns = Vec::new();
-    let mut total_pattern_height = 0;
     let mut state_map = HashMap::new();
 
     while dropped_rocks < rock_count {
@@ -147,7 +145,7 @@ pub fn height_for_dropped_rocks(chamber: &mut Chamber, push_list: Vec<PushDirect
                     
                 },
                 PushDirection::Right => {
-                    if rock_position.column + width < chamber.width - 1 {
+                    if rock_position.column + width < chamber.width as u64 - 1 {
                         rock_position.column += 1;
                     
                         if chamber.collides(&rock_position, &rock_shape) {
@@ -170,97 +168,52 @@ pub fn height_for_dropped_rocks(chamber: &mut Chamber, push_list: Vec<PushDirect
             }
         }
         
-        if dropped_rocks % 1000 == 0 {
-            state_map.insert(create_state(chamber, jet_index, 5), dropped_rocks);
-        }
+        let state = create_state(chamber, jet_index, (dropped_rocks % 5) as usize);
         
-        if total_pattern_height == 0 {
-            if let Some(pattern) = find_pattern(chamber) {
-                found_patterns.push((pattern, dropped_rocks, jet_index));
-            
-                if let Some((before_rocks, pattern_rocks, before_height, pattern_height, dropped_counter)) = pattern_repeats(&found_patterns) {
-                    let pattern_count = (rock_count - before_rocks) / pattern_rocks;
-                    total_pattern_height =  before_height + pattern_height * pattern_count as usize;
-                    dropped_rocks = before_rocks + pattern_count * pattern_rocks;
-                    jet_index = dropped_counter;
-                }
+        if dropped_rocks % 10000 == 0 {
+            dbg!(&state);
+            dbg!(state_map.len());
+        }
+
+        if state_map.contains_key(&state) {
+            dbg!(state);
+            todo!("PLS HELP")
+        } else {
+            state_map.insert(state, (dropped_rocks, chamber.highest_rock()));
+        }
+    }
+    
+    chamber.highest_rock()
+}
+
+fn create_state(chamber: &Chamber, jet_index: usize, rock_index: usize) -> State {
+    let mut rock_distance = Vec::new();
+    
+    for i in 0..chamber.width {
+        for j in (0..chamber.filled_space.len()).rev() {
+            if chamber.filled_space[j][i] == Filling::Rock {
+                rock_distance.push(chamber.filled_space.len() - (j + 1));
+                break;
             }
         }
-    }
-    
-    total_pattern_height as u64 + chamber.highest_rock()
-}
-
-fn create_state(chamber: &Chamber, jet_index: usize, top_layers_count: usize) -> State {
-    let mut top_layers = vec![Vec::new(); top_layers_count];
-    let start_layer = chamber.filled_space.len() - (top_layers_count + 1);
-    
-    top_layers.clone_from_slice(&chamber.filled_space[start_layer..]);
-    
-    State { top_layers, jet_index, height: chamber.filled_space.len() }
-}
-
-fn find_pattern(chamber: &mut Chamber) -> Option<Vec<Vec<Filling>>> {
-    
-    for i in 0..chamber.filled_space.len() {
-        if !chamber.filled_space[i].contains(&Filling::Air) {
-            let mut pattern = vec![Vec::new(); i + 1];
-            pattern.clone_from_slice(&chamber.filled_space[0..=i]);
-            
-            let remaining_len = chamber.filled_space.len() - (i + 1);
-            if remaining_len > 0 {
-                let mut rest = vec![Vec::new(); remaining_len];
-                rest.clone_from_slice(&chamber.filled_space[i + 1..]);
-                
-                chamber.filled_space = rest;
-            } else {
-                chamber.filled_space = Vec::new()
-            }
-
-            return Some(pattern)
-        }
-    }
-
-    None
-}
-
-fn pattern_repeats(patterns: &Vec<(Vec<Vec<Filling>>, u64, usize)>) -> Option<(u64, u64, usize, usize, usize)> {
-    for i in 0..&patterns.len() - 1 {
-        let last = &patterns[patterns.len() - 1]; 
-        let previous = &patterns[i];
         
-        if last.0 == previous.0 && last.2 == previous.2 {
-            dbg!(i);
-            dbg!(patterns.len() - 1);
-            let before_height: usize = patterns[..i].iter().map(|pattern| pattern.0.len()).sum();
-            let before_rocks = if i == 0 {
-                0
-            } else {
-                patterns[i].1
-            };
-            let repition_height: usize = patterns[i..patterns.len()].iter().map(|pattern| pattern.0.len()).sum();
-            let repition_rocks: u64 = patterns[patterns.len() - 1].1 - before_rocks;
-
-            dbg!(before_height);
-            dbg!(before_rocks);
-
-            dbg!(repition_height);
-            dbg!(repition_rocks);
-
-            return Some((before_rocks, repition_rocks, before_height, repition_height, last.2))
+        if rock_distance.len() < i + 1 {
+            rock_distance.push(chamber.highest_rock() as usize);
         }
     }
-
-    None
+    
+    assert_eq!(rock_distance.len(), chamber.width);
+    
+    State { rock_distance, jet_index, rock_index }
 }
 
 pub struct Chamber {
-    width: u64,
+    width: usize,
     filled_space: Vec<Vec<Filling>>,
 }
 
 impl Chamber {
-    pub fn new(width: u64) -> Self {
+    pub fn new(width: usize) -> Self {
         Self { width, filled_space: Vec::new() }
     }
 
@@ -299,9 +252,9 @@ impl Chamber {
     }
 }
 
-#[derive(Hash, PartialEq, Eq)]
+#[derive(Debug, Hash, PartialEq, Eq)]
 struct State {
-    top_layers: Vec<Vec<Filling>>,
+    rock_distance: Vec<usize>,
     jet_index: usize,
-    height: usize,
+    rock_index: usize,
 }
